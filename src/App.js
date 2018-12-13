0dc8b872ac98ea0950c18eb85332d3efde72face
kbatuigas <kbatuigas@gmail.com>
2018-12-12 21:33:12 -0800

    Enable BooksAPI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 import MainPage from './components/pages/MainPage.js'
