baf0fa48c7f10a86684ce8b7c206c9ba0797f87d
kbatuigas <kbatuigas@gmail.com>
2019-01-13 21:44:27 -0800

    Add results and query to SearchPage state

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -7,7 +7,9 @@ class SearchPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        books: []
+        books: [],
+        results: [],
+        query: ""
     }
   }
 
@@ -43,7 +45,7 @@ class SearchPage extends React.Component {
                 However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                 you don't find a specific author or title. Every search is limited by search terms.
               */}
-              <input type="text" placeholder="Search by title or author"/>
+              <input type="text" placeholder="Search by title or author"/ >
 
             </div>
           </div>
